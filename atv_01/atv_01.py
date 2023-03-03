73a7392da39ff4e02c3788dce813b3e190376114
Temistocles Zwang <61996692+TemistoclesZwang@users.noreply.github.com>
2023-03-03 15:22:10 -0300

    Update atv_01.py

--- atv_01/atv_01.py
+++ atv_01/atv_01.py
@@ -2,20 +2,11 @@ import requests
 import requests_cache
 from bs4 import BeautifulSoup
 import re
-import sqlite3
 
-# requests_cache.install_cache('banco') #! desativar para localhost
+requests_cache.install_cache('banco') #! desativar para localhost
+# requests_cache.install_cache('banco', expire_after= 60)
 
-def banco():
-   con = sqlite3.connect('/home/temiszwang/Desktop/codigos/temp web 1/banco.sqlite')
-   cur = con.cursor()
-   return cur.fetchall()
-
-
-def auto_cita(link):
-   print('')
-
-
+################### ORGANIZADORES ###################
 ranking = {}
 def response_and_soup(link):
    response = requests.get(link,verify=False)
@@ -23,51 +14,81 @@ def response_and_soup(link):
    return soup
 
 
-def organizar_ranking ():
+def criterio_auto_citacao(link_inicial):
+   #se tiver o site original, pinta vermelho e no fim
+   padrao = r"(https?://)(.*?\.com)"
+   extrair_dominio = re.search(padrao, link_inicial)
+   dominio = extrair_dominio.group(1)\
+         +extrair_dominio.group(2)
+
+   for site in list(ranking):
+      if dominio in site:
+         #mover para o final
+         item_a = ranking.pop(site)
+         ranking['🔴'+site] = item_a
+      else:
+         valor = ranking.pop(site)
+         ranking['🟢'+site] = valor
+   separa_verde_vermelho()
+
+
+def separa_verde_vermelho():
+   verde = {}
+   vermelho = {}
+
+   for site in list(ranking):
+      if '🟢' in site:
+         valor = ranking.pop(site)
+         verde.update({site: valor})
+      else:
+         valor = ranking.pop(site)
+         vermelho.update({site: valor})
+   
+   print('\n👍 MAISrelevantes')
+   organizar_ranking_online (verde)
+
+   print('\n👎 MENOS relevantes')
+   organizar_ranking_online (vermelho)
+
+
+def organizar_ranking_online (dicionario):
+   organizar = dict(sorted(dicionario.items(), \
+      key=lambda x: x[1], reverse=True))
+   
+   # print('\n🏆 Ranking 🏆:')
+   [print(f"{site}: {ocorrencia}") \
+      for site, ocorrencia in organizar.items()]
+      
+
+def organizar_ranking_local_host ():
    organizar = dict(sorted(ranking.items(), \
       key=lambda x: x[1], reverse=True))
+   
    print('\n🏆 Ranking 🏆:')
    [print(f"{site}: {ocorrencia}") \
       for site, ocorrencia in organizar.items()]
 
 
-def criterio_google(link,keyword):
-   #procura pela palavra no google, se o site estiver entre os primeiros
-   #resultados então ganha ponto
-   soup = response_and_soup(f'https://www.google.com/search?q={keyword}')
-   todos_os_links = soup.find_all('a',href=True)#!
-
-   for link_da_lista in todos_os_links:
-      link_extraido = link_da_lista['href']
-      if 'http' in link_extraido:
-         nome_do_site = re.compile(r'www\.(\w+)\.com')
-         match = nome_do_site.search(link_extraido)
-
-         if match:
-            ranking.update({link:1})
-            #breakpoint em cima para ver nome do site
-            return 0
-
 controle_de_loop = []
 tmp = 0 
 links_teste = []
 
-def profundidade_padrao(keyword,link):
+################### PROFUNDIDADES ###################
+def profundidade_padrao(keyword,link,limitar_resultados=0):
+   
    if link not in controle_de_loop:
       print('\n🔎 procurando termo em:',link)
       se_tem_keyword(keyword,link)
-   
       soup = response_and_soup(link)
       #retorna os links dentro do link passado
-      todos_os_links = soup.find_all('a',href=True)#!
+      todos_os_links = soup.find_all('a',href=True,limit=limitar_resultados)#!
       
       for link_da_lista in todos_os_links:
          link_extraido = link_da_lista['href']
-         if link_extraido.startswith('http'):
+         if link_extraido.startswith('http'): 
             print('        links encontrados',link_extraido)
             links_teste.append(link_da_lista)
             links_referencias.append(link)
-
    else:
       links_referencias.append(link)
 
@@ -90,21 +111,40 @@ def profundidade_recursiva(keyword,tmp,prof):
       profundidade_recursiva(keyword,tmp,prof)
 
 
+################### CRITERIOS ###################
+
 def criterio_referencias(links_referencias):
-   #como verifica apontamentos só funciona com profundidade >0
+   #como verifica apontamentos só funciona com profundidade > 0
    for item in links_referencias:
       repeticao = links_referencias.count(item) #quantas vezes repetiu
-      if repeticao > 1 and item not in ranking:
-         ranking.update({item:repeticao})
+      
+      if repeticao > 1:
+         # ranking.update({item:repeticao})
+         ranking[item]=ranking[item]+1
 
 
+def verifica_link_existe_no_ranking(link):
+   if link not in ranking:
+      ranking.update({link:1})
+
+
+def criterio_add(link):
+   verifica_link_existe_no_ranking(link)
+   ranking[link]=ranking[link]+1
+
+
+def criterio_sub(link):
+   verifica_link_existe_no_ranking(link)
+   if ranking[link] > 1: 
+      ranking[link]=ranking[link]-1
+
+################### CHARS ANTERS E DEPOIS ###################
 def chars_antes_e_depois(posicao,lista_palavras):
-   char_antes = lista_palavras[posicao-20:posicao]
-   char_depois = lista_palavras[posicao:posicao+20]
+   char_antes = lista_palavras[posicao-30:posicao]
+   char_depois = lista_palavras[posicao:posicao+30]
    print('  💬',char_antes,char_depois)
 
-
-def se_tem_keyword(termo,link):
+def formatar_lista_palavras(termo,link):
    controle_de_loop.append(link)
    soup = response_and_soup(link)
 
@@ -112,25 +152,46 @@ def se_tem_keyword(termo,link):
    gerar_string_de_busca = ''.join(lista)
    checar_termo = gerar_string_de_busca.find(termo)
 
-   if checar_termo != -1:
+   return checar_termo,gerar_string_de_busca
 
+################### SE TEM O TERMO ###################
+def se_tem_keyword(termo,link):
+   checar_termo,gerar_string_de_busca = formatar_lista_palavras(termo,link)
+   
+   if checar_termo != -1:
       print('  ✅ -> termo encontrado')
       chars_antes_e_depois(checar_termo,gerar_string_de_busca)
+      criterio_add(link)
       return True
 
    else:
       print('  ❌ -> termo nao encontrado')
+      criterio_sub(link)
       return False
 
 
+################### INICIO ###################
 def search(keyword,url,depth):
-   if depth > 2:
+   if depth > 5:
       print('insira uma profundidade menor')
    else:
-      profundidade_padrao(keyword,url)
+      profundidade_padrao(keyword,url) #limitando resultados
       profundidade_recursiva(keyword,tmp,depth)
       criterio_referencias(links_referencias)
-      organizar_ranking () 
+      # organizar_ranking_local_host () 
+      criterio_auto_citacao(url)
 
-# search ('Help','http://127.0.0.1:5500/Programacao_para_internet_1/atv_0/pagina1.html',2)
-search ('Brasil','https://news.google.com/home?hl=pt-BR&gl=BR&ceid=BR:pt-419',1)
+# search ('Help','http://127.0.0.1:5500/Programacao_para_internet_1/atv_0/pagina1.html',1)
+
+# search ('Lula','https://www.bbc.com/portuguese',4)
+search ('Brasil','https://www.infomoney.com.br/',3)
+# search ('Brasil','https://www.folha.uol.com.br/',0)
+
+
+#devido ao não uso de links internos definido na linha 91
+   #o código se sai melhor com grandes profundidades 
+   #(já que existe um limitador na linha 87)
+   #por isso a performance até a profundidade 4 é relativamente boa
+#em baixas profundidades a quantidade de links internos é muito grande
+#em altas é menor e melhora o ranking final
+
